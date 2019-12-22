c26385f35e0c996396a6b4f5e62e312825d74b92
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-12-22 10:36:30 +0800

    Make HRR take in a vector size argument

--- HRR.py
+++ HRR.py
@@ -1,155 +1,169 @@
 from utils import persist_to_file, normalize
-from AST import Var, Const, Func, Eq, Disj, Conj
+from AST import Var, Const, Dist, Func, Eq, Disj, Conj
 import numpy as np
-
-VEC_DIM = 512
+import functools
+import zlib
 
 def associate(x, y):
     """ Create association between two objects """
     # Stolen from:
     # http://www.indiana.edu/~clcl/holoword/Site/__files/holoword.py
     z = np.fft.ifft(np.fft.fft(x) * np.fft.fft(y)).real
-    if np.ndim(z) == 1:
-        z = z[None, :]
-    return z
+    return normalize(z)
 
 def merge2(x, y, weight=0.5):
     """ Merge two associations together """
+    z = normalize(weight * x + (1-weight) * y)
     return normalize(weight * x + (1-weight) * y)
 
 def merge(it):
     """ Merge a bag of associations together """
-    s = it[0]
+    s = it[0].copy()
     for i in it[1:]:
         s += i
+    s = normalize(s)
     return normalize(s)
 
 def involution(x):
-    """ Flips array """
-    if np.ndim(x) == 1:
-        x = x[None, :]
-    return np.concatenate([x[:, None, 0], x[:, -1:0:-1]], 1)
-
+    """ Flips array from second element onwards """
+    return np.concatenate((x[0:1], x[-1:0:-1]))
 
 def decode(x, xy):
     """ (Attempts to) Decode association of two objects """
     return circular_convolution(involution(x), xy)
 
-@persist_to_file('HRR_ground_vecs_{}.pickle'.format(VEC_DIM))
-def get_ground_vector(label):
+@functools.lru_cache()
+def get_ground_vector(hrr_size, label):
+    """ Deterministically generate a random vector from its label """
+    rs = np.random.RandomState(
+            zlib.adler32(
+                (str(hrr_size)+label).encode('utf-8')
+                ) & 0xffffffff)
+    rs.randint(2)
+
     if ':' in label:
         parent, _, specifier = label.rpartition(':')
-        return merge2(get_ground_vector(parent), normalize(np.random.standard_normal(VEC_DIM)))
+        parentvec = get_ground_vector(hrr_size, parent)
+        newvec = merge2(parentvec, normalize(rs.standard_normal(hrr_size)))
+        return newvec
     else:
-        return normalize(np.random.standard_normal(VEC_DIM))
+        return normalize(rs.standard_normal(hrr_size))
 
 class HRR:
     """ Interface for HRR manipulations """
 
-    @classmethod
-    def fold_term(cls, term):
+    def __init__(self, hrr_size):
+        self.hrr_size = hrr_size
+
+    @functools.lru_cache(maxsize=8192)
+    def fold_term(self, term):
         """ Outputs a HRR vector given a FOF term """
         if isinstance(term, Var):
-            return cls.var(term.name)
+            return self.var(term.name)
         elif isinstance(term, Const):
-            return cls.const(term.name)
+            return self.const(term.name)
+        elif isinstance(term, Dist):
+            return self.dist(term.name)
         elif isinstance(term, Func):
-            return cls.func(
+            return self.func(
                     term.funcname,
-                    [ fold_term(v, c, f, x)
+                    [ self.fold_term(x)
                       for x in term.args ])
 
-    @classmethod
-    def encode(cls, conj):
+    def encode_eq(self, eq):
+        """ Outputs a HRR vector given a literal """
+        return self.eq(
+                eq.pos,
+                self.fold_term(eq.t1),
+                self.fold_term(eq.t2))
+
+    def encode_disj(self, disj):
+        """ Outputs a HRR vector given a disjunction of literals """
+        return self.disj(
+                disj.role,
+                [ self.encode_eq(eq)
+                  for eq in disj.eqs ])
+
+    def encode_conj(self, conj):
         """ Outputs a HRR vector given a CNF formula """
-        return cls.conj(
-                [ cls.disj(
-                    disj.role,
-                    [ cls.eq(
-                        eq.pos,
-                        cls.fold_term(eq.t1),
-                        cls.fold_term(eq.t2))
-                      for eq in disj.eqs ])
+        return self.conj(
+                [ self.encode_disj(disj)
                   for disj in conj.disjs ])
 
-    @staticmethod
-    def var(name):
+    def var(self, name):
         """ Output a HRR vector given a variable name """
         raise NotImplementedError
 
-    @staticmethod
-    def const(name):
+    def const(self, name):
         """ Output a HRR vector given a constant name """
         raise NotImplementedError
 
-    @staticmethod
-    def func(name, vecs):
+    def dist(self, name):
+        """ Output a HRR vector given a distinct object name """
+        raise NotImplementedError
+
+    def func(self, name, vecs):
         """ Output a HRR vector given function name and HRR vectors of its inputs """
         raise NotImplementedError
 
-    @staticmethod
-    def eq(pos, vec1, vec2):
+    def eq(self, pos, vec1, vec2):
         """ Output a HRR vector given positivity of this literal (literals are equations), and HRR vectors of the left and right sides """
         raise NotImplementedError
 
-    @staticmethod
-    def disj(role, vecs):
+    def disj(self, role, vecs):
         """ Output a HRR vector given role of this clause, and HRR vectors of all the literals """
         raise NotImplementedError
 
-    @staticmethod
-    def conj(vecs):
+    def conj(self, vecs):
         """ Output a HRR vector given HRR vectors of all the clauses """
         raise NotImplementedError
 
 class FlatTreeHRR(HRR):
-    @staticmethod
-    def var(name):
+    def var(self, name):
         """ Output a HRR vector given a variable name """
-        return get_ground_vector('Var:{}'.format(name))
+        return get_ground_vector(self.hrr_size, 'Var:{}'.format(name))
 
-    @staticmethod
-    def const(name):
+    def const(self, name):
         """ Output a HRR vector given a constant name """
-        return get_ground_vector('Const:{}'.format(name))
+        return get_ground_vector(self.hrr_size, 'Const:{}'.format(name))
 
-    @staticmethod
-    def func(name, vecs):
+    def dist(self, name):
+        """ Output a HRR vector given a distinct object name """
+        return get_ground_vector(self.hrr_size, 'Dist:{}'.format(name))
+
+    def func(self, name, vecs):
         """ Output a HRR vector given function name and HRR vectors of its inputs """
         arity = len(vecs)
-        funcobj = get_ground_vector('Func:{}:{}'.format(arity, name))
+        funcobj = get_ground_vector(self.hrr_size, 'Func:{}:{}'.format(arity, name))
 
         argobjs = [
                 associate(
-                    get_ground_vector('Arg:{}:{}:{}'.format(arity, name, i)),
+                    get_ground_vector(self.hrr_size, 'Arg:{}:{}:{}'.format(arity, name, i)),
                     vec)
                 for i, vec in enumerate(vecs) ]
-        return merge([funcobj, merge(argobjs), merge(vecs)])
+        result = merge([funcobj, merge(argobjs), merge(vecs)])
+        return result
 
-    @staticmethod
-    def eq(pos, vec1, vec2):
+    def eq(self, pos, vec1, vec2):
         """ Output a HRR vector given positivity of this literal (literals are equations), and HRR vectors of the left and right sides """
         return merge([
             merge2(
-                associate(get_ground_vector('Eq:{}:Left'.format(pos)), vec1),
-                associate(get_ground_vector('Eq:{}:Right'.format(pos)), vec2)),
+                associate(get_ground_vector(self.hrr_size, 'Eq:{}:Left'.format(pos)), vec1),
+                associate(get_ground_vector(self.hrr_size, 'Eq:{}:Right'.format(pos)), vec2)),
             vec1,
             vec2])
 
-    @staticmethod
-    def disj(role, vecs):
+    def disj(self, role, vecs):
         """ Output a HRR vector given role of this clause, and HRR vectors of all the literals """
-        roleobj = get_ground_vector('DisjRole:{}'.format(role))
+        roleobj = get_ground_vector(self.hrr_size, 'DisjRole:{}'.format(role))
         objs = [ associate(roleobj, vec) for vec in vecs ]
         merged = merge(objs)
         disjobj = associate(merged, merged) # Essentially associates each literal with each other
         return merge2(disjobj, merge(vecs))
 
-    @staticmethod
-    def conj(vecs):
+    def conj(self, vecs):
         """ Output a HRR vector given HRR vectors of all the clauses """
         return merge(vecs)
-
 
 # class TreeHRR(HRR):
 
--- utils.py
+++ utils.py
@@ -1,23 +1,57 @@
+import sys
 import pickle
+import functools
 
-def persist_to_file(file_name):
+import numpy as np
+
+def active_persist_to_file(filename):
     def decorator(original_func):
-        try:
-            cache = pickle.load(open(file_name, 'rb'))
-        except (IOError, ValueError):
-            cache = {}
+        cache = None
 
-        def new_func(param):
-            if param not in cache:
-                cache[param] = original_func(param)
-                pickle.dump(cache, open(file_name, 'wb'))
-            return cache[param]
+        @functools.wraps(original_func)
+        def new_func(*params):
+            nonlocal cache
+
+            if cache is None:
+                try:
+                    cache = pickle.load(open(filename, 'rb'))
+                except (IOError, ValueError):
+                    cache = {}
+
+            if params not in cache:
+                cache[params] = original_func(*params)
+                try:
+                    pickle.dump(cache, open(filename, 'wb'))
+                except:
+                    sys.stderr.write("Couldn't save {}!".format(filename))
+            return cache[params]
 
         return new_func
 
     return decorator
 
-import numpy as np
+def persist_to_file(filename):
+    def decorator(original_func):
+
+        @functools.wraps(original_func)
+        def new_func(*params):
+            # print('Loading', params, 'from', filename.format(*params))
+            try:
+                cache = pickle.load(open(filename.format(*params), 'rb'))
+            except (IOError, ValueError):
+                cache = {}
+
+            if params not in cache:
+                cache[params] = original_func(*params)
+                try:
+                    pickle.dump(cache, open(filename.format(*params), 'wb'))
+                except:
+                    sys.stderr.write("Couldn't save {}!".format(filename.format(*params)))
+            return cache[params]
+
+        return new_func
+
+    return decorator
 
 def normalize(vec):
     return vec / np.linalg.norm(vec)
